d0c5a071e854cdd28e65a53b3e1e0f1c49036f1e
杨旋 <364105996@qq.com>
2018-08-16 17:14:26 +0800

    - protobuf 测试

--- test_mqtt/test_mqtt.go
+++ test_mqtt/test_mqtt.go
@@ -2,98 +2,25 @@ package main
 
 import (
 	"GoLab/test_mqtt/work"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"strings"
-
-	goprotobuf "GoLab/test_protobuf/proto"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
-	"github.com/golang/protobuf/proto"
 
-	"os"
-	"os/signal"
-	"strconv"
 	"sync"
 	"time"
 )
 
 func main() {
 	// doConn(nil, "-1", false)
-	// doMultiConn()
-	// doConnWithInput()
-	test_protobuf()
+	// test_multiJson(10, true)
+	// test_multiProtobuf(1, true)
+	// test_multiProtobuf(10, true)
+	// test_multiProtobuf(10, true)
+	ArgsParser()
 }
 
-func doMultiConn() {
-	var num int
-	flag.IntVar(&num, "num", -1, "help msg for name")
-	flag.Parse()
-
-	var wg sync.WaitGroup
-	cnt := 3
-
-	if num > 0 {
-		cnt = num
-	}
-
-	for index := 1; index <= cnt; index++ {
-		wg.Add(1)
-		go doConn(&wg, strconv.Itoa(index), true)
-	}
-
-	wg.Wait()
-	fmt.Println("程序结束 666")
-}
-
-func doConn(wg *sync.WaitGroup, flag string, isPing bool) {
-	c := make(chan os.Signal, 1)
-
-	this := new(work.MqttWork)
-	opts := this.GetDefaultOptions("tcp://127.0.0.1:3563")
-	opts.SetConnectionLostHandler(func(client MQTT.Client, err error) {
-		fmt.Println("连接断开", err.Error())
-		c <- os.Kill
-	})
-	opts.SetOnConnectHandler(func(client MQTT.Client) {
-		fmt.Println("连接成功")
-	})
-	err := this.Connect(opts)
-	if err != nil {
-		fmt.Println("连接错误:", err.Error())
-		c <- os.Kill
-	}
-
-	cnt := 1
-	sendFn := func() {
-		//访问HelloWorld001模块的HD_Say函数
-		hiStr := fmt.Sprintf(`{"say":"我是wilker%s, cnt:%d"}`, flag, cnt)
-		msg, err := this.Request("HelloWorld@HelloWorld001/HD_Say", []byte(hiStr))
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		fmt.Println(fmt.Sprintf("topic :%s  body :%s", msg.Topic(), string(msg.Payload())))
-	}
-	sendFn()
-
-	if isPing {
-		for {
-			cnt++
-			sendFn()
-			time.Sleep(time.Second * 3)
-		}
-	}
-
-	signal.Notify(c, os.Interrupt, os.Kill)
-	sig := <-c
-	fmt.Printf("mqant closing down (signal: %v)\n", sig)
-	fmt.Println("--- flag done:", flag)
-	if wg != nil {
-		defer wg.Done()
-	}
-}
-
+/*
 func doConnWithInput() {
 	rInputFn := func(c chan string) {
 		for {
@@ -166,18 +93,23 @@ func doConnWithInput() {
 	sig := <-c
 	fmt.Printf("mqant closing down (signal: %v)\n", sig)
 }
+*/
 
-type ResBody struct {
-	Error  string
-	Result []byte
+type JsonSend struct {
+	topic string
+	buff  string
 }
 
-func test_protobuf() {
+func tcp_json(wg *sync.WaitGroup, ps chan *JsonSend) {
+	defer wg.Done()
+
+	sig := make(chan bool, 1)
+
 	this := new(work.MqttWork)
 	opts := this.GetDefaultOptions("tcp://127.0.0.1:3563")
 	opts.SetConnectionLostHandler(func(client MQTT.Client, err error) {
 		fmt.Println("连接断开", err.Error())
-		// c <- os.Kill
+		sig <- true
 	})
 	opts.SetOnConnectHandler(func(client MQTT.Client) {
 		fmt.Println("连接成功")
@@ -185,44 +117,97 @@ func test_protobuf() {
 	err := this.Connect(opts)
 	if err != nil {
 		fmt.Println("连接错误:", err.Error())
-		// c <- os.Kill
+		return
 	}
 
-	sendFn := func(topic string, body []byte) {
+	sendFn := func(topic string, body string) {
 		if topic == "" {
-			topic = "HelloWorld@HelloWorld001/HD_Walk"
+			sig <- true
+			return
 		}
-		if body == nil {
-			body = []byte("Sorry msg")
-		}
-		msg, err := this.Request(topic, body)
+
+		hiStr := fmt.Sprintf(`{"say":"msg:%s"}`, body)
+		msg, err := this.Request(topic, []byte(hiStr))
 		if err != nil {
-			fmt.Println(err.Error())
+			fmt.Println("返回消息失败, ", err.Error())
+			sig <- true
+			return
 		}
 
-		retmsg := &goprotobuf.HelloWorld{}
-		retBytes := msg.Payload()
+		// 解码 mqtt 消息体
+		// rb := &ResBody{}
+		// retBytes := msg.Payload()
+		// if err = json.Unmarshal([]byte(retBytes), &rb); err != nil {
+		// 	fmt.Println("mqtt json 解码失败:", rb)
+		// 	sig <- true
+		// 	return
+		// }
 
-		rb := &ResBody{}
+		fmt.Println("返回结果:", string(msg.Payload()))
+	}
 
-		if err = json.Unmarshal([]byte(retBytes), &rb); err != nil {
-			fmt.Println("mqtt json 解码失败:", rb)
-		}
-
-		if err = proto.Unmarshal(rb.Result, retmsg); err == nil {
-			fmt.Println("len:", len(rb.Result), "bytes:", rb.Result)
-			fmt.Println(fmt.Sprintf("msgId:%d, topic:%s, body:%s\n", msg.MessageID(), msg.Topic(), retmsg.String()))
+	for {
+		select {
+		case sendBody := <-ps:
+			if sendBody != nil {
+				sendFn(sendBody.topic, sendBody.buff)
+			} else {
+				sig <- true
+			}
+		case <-sig:
+			fmt.Printf("结束发送:(signal: %v)\n", sig)
+			return
 		}
 	}
 
-	msg := &goprotobuf.HelloWorld{
-		Id:  proto.Int32(996),
-		Str: proto.String("what the fuck"),
+}
+
+// 多开客户端数量, 是否定时发送
+func test_multiJson(num int, isPing bool) {
+	// c := make(chan os.Signal, 1)
+	var wg sync.WaitGroup
+
+	createJsFn := func(flag int, sendCnt int) *JsonSend {
+		return &JsonSend{
+			topic: "HelloWorld@HelloWorld001/HD_Say",
+			buff:  fmt.Sprintf("wilker-%d , call cnt:%d", flag, sendCnt),
+		}
 	}
 
-	if buffer, err := proto.Marshal(msg); err == nil {
-		sendFn("HelloWorld@HelloWorld001/HD_Walk", buffer)
+	createTcpFn := func(flag int) {
+		js := make(chan *JsonSend, 1)
+		go tcp_json(&wg, js)
+
+		cnt := 1
+		for {
+			ctorJs := createJsFn(flag, cnt)
+			js <- ctorJs
+			if !isPing || ctorJs == nil {
+				break
+			}
+			cnt++
+			time.Sleep(time.Second * 3)
+		}
 	}
 
-	select {}
+	for index := 1; index <= num; index++ {
+		wg.Add(1)
+		go createTcpFn(index)
+	}
+
+	wg.Wait()
+	fmt.Println("GameOver!!!")
+}
+
+func ArgsParser() {
+	num := flag.Int("num", 10, "arg is num")
+	isPing := flag.Bool("isPing", true, "arg is isPing")
+	isProto := flag.Bool("isProto", true, "arg is isProto")
+	flag.Parse()
+
+	if *isProto {
+		test_multiProtobuf(*num, *isPing)
+	} else {
+		test_multiJson(*num, *isPing)
+	}
 }
---
+++ test_mqtt/test_mqtt_protobuf.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"GoLab/test_mqtt/work"
+	"encoding/json"
+	"fmt"
+
+	goprotobuf "GoLab/test_protobuf/proto"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"github.com/golang/protobuf/proto"
+
+	"sync"
+	"time"
+)
+
+type ResBody struct {
+	Error  string
+	Result []byte
+}
+
+type ProtoSend struct {
+	topic string
+	buff  []byte
+}
+
+func tcp_protobuf(wg *sync.WaitGroup, ps chan *ProtoSend) {
+	defer wg.Done()
+
+	sig := make(chan bool, 1)
+
+	this := new(work.MqttWork)
+	opts := this.GetDefaultOptions("tcp://127.0.0.1:3563")
+	opts.SetConnectionLostHandler(func(client MQTT.Client, err error) {
+		fmt.Println("连接断开", err.Error())
+		sig <- true
+	})
+	opts.SetOnConnectHandler(func(client MQTT.Client) {
+		fmt.Println("连接成功")
+	})
+	err := this.Connect(opts)
+	if err != nil {
+		fmt.Println("连接错误:", err.Error())
+		return
+	}
+
+	sendFn := func(topic string, body []byte) {
+		if topic == "" {
+			sig <- true
+			return
+		}
+		if body == nil {
+			sig <- true
+			return
+		}
+
+		msg, err := this.Request(topic, body)
+		if err != nil {
+			fmt.Println("返回消息失败, ", err.Error())
+			sig <- true
+			return
+		}
+
+		// 解码 mqtt 消息体
+		rb := &ResBody{}
+		retBytes := msg.Payload()
+		if err = json.Unmarshal([]byte(retBytes), &rb); err != nil {
+			fmt.Println("mqtt json 解码失败:", rb)
+			sig <- true
+			return
+		}
+
+		// 解码 protobuf 消息体
+		retmsg := &goprotobuf.HelloWorld{}
+		if err = proto.Unmarshal(rb.Result, retmsg); err == nil {
+			// fmt.Println("len:", len(rb.Result), "bytes:", rb.Result)
+			fmt.Println(fmt.Sprintf("msgId:%d, topic:%s, body:%s", msg.MessageID(), msg.Topic(), retmsg.String()))
+		}
+	}
+
+	for {
+		select {
+		case sendBody := <-ps:
+			if sendBody != nil {
+				sendFn(sendBody.topic, sendBody.buff)
+			} else {
+				sig <- true
+			}
+		case <-sig:
+			fmt.Printf("结束发送:(signal: %v)\n", sig)
+			return
+		}
+	}
+
+}
+
+// 多开客户端数量, 是否定时发送
+func test_multiProtobuf(num int, isPing bool) {
+	// c := make(chan os.Signal, 1)
+	var wg sync.WaitGroup
+
+	createPsFn := func(flag int, sendCnt int) *ProtoSend {
+		msg := &goprotobuf.HelloWorld{
+			Id:  proto.Int32(996),
+			Str: proto.String(fmt.Sprintf("wilker-%d , call cnt:%d", flag, sendCnt)),
+		}
+
+		buffer, err := proto.Marshal(msg)
+		if err != nil {
+			return nil
+		}
+
+		return &ProtoSend{
+			topic: "HelloWorld@HelloWorld001/HD_Walk",
+			buff:  buffer,
+		}
+	}
+
+	createTcpFn := func(flag int) {
+		ps := make(chan *ProtoSend, 1)
+		go tcp_protobuf(&wg, ps)
+
+		cnt := 1
+		for {
+			ctorPs := createPsFn(flag, cnt)
+			ps <- ctorPs
+			if !isPing || ctorPs == nil {
+				break
+			}
+			cnt++
+			time.Sleep(time.Second * 3)
+		}
+	}
+
+	for index := 1; index <= num; index++ {
+		wg.Add(1)
+		go createTcpFn(index)
+	}
+
+	wg.Wait()
+	fmt.Println("GameOver!!!")
+}
+
+/*
+func test_protobuf() {
+	this := new(work.MqttWork)
+	opts := this.GetDefaultOptions("tcp://127.0.0.1:3563")
+	opts.SetConnectionLostHandler(func(client MQTT.Client, err error) {
+		fmt.Println("连接断开", err.Error())
+		// c <- os.Kill
+	})
+	opts.SetOnConnectHandler(func(client MQTT.Client) {
+		fmt.Println("连接成功")
+	})
+	err := this.Connect(opts)
+	if err != nil {
+		fmt.Println("连接错误:", err.Error())
+		// c <- os.Kill
+	}
+
+	sendFn := func(topic string, body []byte) {
+		if topic == "" {
+			topic = "HelloWorld@HelloWorld001/HD_Walk"
+		}
+		if body == nil {
+			body = []byte("Sorry msg")
+		}
+		msg, err := this.Request(topic, body)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		retmsg := &goprotobuf.HelloWorld{}
+		retBytes := msg.Payload()
+
+		rb := &ResBody{}
+
+		if err = json.Unmarshal([]byte(retBytes), &rb); err != nil {
+			fmt.Println("mqtt json 解码失败:", rb)
+		}
+
+		if err = proto.Unmarshal(rb.Result, retmsg); err == nil {
+			fmt.Println("len:", len(rb.Result), "bytes:", rb.Result)
+			fmt.Println(fmt.Sprintf("msgId:%d, topic:%s, body:%s", msg.MessageID(), msg.Topic(), retmsg.String()))
+		}
+	}
+
+	msg := &goprotobuf.HelloWorld{
+		Id:  proto.Int32(996),
+		Str: proto.String("what the fuck"),
+	}
+
+	if buffer, err := proto.Marshal(msg); err == nil {
+		sendFn("HelloWorld@HelloWorld001/HD_Walk", buffer)
+	}
+
+	select {}
+}
+*/
