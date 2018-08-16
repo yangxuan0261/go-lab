a27f3ce63277c42e0204dbeb3551c4f631fc4bd5
杨旋 <364105996@qq.com>
2018-08-16 15:11:48 +0800

    - 增加 protobuf 消息测试

--- test_mqtt/test_mqtt.go
+++ test_mqtt/test_mqtt.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"GoLab/test_mqtt/work"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"strings"
 
+	goprotobuf "GoLab/test_protobuf/proto"
+
 	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"github.com/golang/protobuf/proto"
 
 	"os"
 	"os/signal"
@@ -16,9 +20,10 @@ import (
 )
 
 func main() {
-	doConn(nil, "-1", false)
+	// doConn(nil, "-1", false)
 	// doMultiConn()
 	// doConnWithInput()
+	test_protobuf()
 }
 
 func doMultiConn() {
@@ -136,7 +141,7 @@ func doConnWithInput() {
 				body = "Sorry msg"
 			}
 			hiStr := fmt.Sprintf(`{"say":"msg:%s"}`, body)
-			msg, err := this.Request("HelloWorld@HelloWorld001/HD_Say", []byte(hiStr))
+			msg, err := this.Request(topic, []byte(hiStr))
 			if err != nil {
 				fmt.Println(err.Error())
 			}
@@ -160,5 +165,64 @@ func doConnWithInput() {
 	signal.Notify(c, os.Interrupt, os.Kill)
 	sig := <-c
 	fmt.Printf("mqant closing down (signal: %v)\n", sig)
+}
 
+type ResBody struct {
+	Error  string
+	Result []byte
+}
+
+func test_protobuf() {
+	this := new(work.MqttWork)
+	opts := this.GetDefaultOptions("tcp://127.0.0.1:3563")
+	opts.SetConnectionLostHandler(func(client MQTT.Client, err error) {
+		fmt.Println("连接断开", err.Error())
+		// c <- os.Kill
+	})
+	opts.SetOnConnectHandler(func(client MQTT.Client) {
+		fmt.Println("连接成功")
+	})
+	err := this.Connect(opts)
+	if err != nil {
+		fmt.Println("连接错误:", err.Error())
+		// c <- os.Kill
+	}
+
+	sendFn := func(topic string, body []byte) {
+		if topic == "" {
+			topic = "HelloWorld@HelloWorld001/HD_Walk"
+		}
+		if body == nil {
+			body = []byte("Sorry msg")
+		}
+		msg, err := this.Request(topic, body)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		retmsg := &goprotobuf.HelloWorld{}
+		retBytes := msg.Payload()
+
+		rb := &ResBody{}
+
+		if err = json.Unmarshal([]byte(retBytes), &rb); err != nil {
+			fmt.Println("mqtt json 解码失败:", rb)
+		}
+
+		if err = proto.Unmarshal(rb.Result, retmsg); err == nil {
+			fmt.Println("len:", len(rb.Result), "bytes:", rb.Result)
+			fmt.Println(fmt.Sprintf("msgId:%d, topic:%s, body:%s\n", msg.MessageID(), msg.Topic(), retmsg.String()))
+		}
+	}
+
+	msg := &goprotobuf.HelloWorld{
+		Id:  proto.Int32(996),
+		Str: proto.String("what the fuck"),
+	}
+
+	if buffer, err := proto.Marshal(msg); err == nil {
+		sendFn("HelloWorld@HelloWorld001/HD_Walk", buffer)
+	}
+
+	select {}
 }
